cfbb122a348502c5c5fc5345ebb6754d5d467928
Wiser-Wong <Wiser_XY_Wong@163.com>
2018-09-08 14:28:08 +0800

    修改Adapter GridLayoutManage footer bug

--- library/src/main/java/com/wiser/library/adapter/WISERRVAdapter.java
+++ library/src/main/java/com/wiser/library/adapter/WISERRVAdapter.java
@@ -317,7 +317,7 @@ public abstract class WISERRVAdapter<T, V extends WISERHolder> extends RecyclerV
 
 	@Override public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
 		super.onAttachedToRecyclerView(recyclerView);
-		if (!isGridLayoutManage(recyclerView)) return;
+		if (!isGridLayoutManage(recyclerView) && !isFooter) return;
 		final GridLayoutManager gridManager = ((GridLayoutManager) recyclerView.getLayoutManager());
 		assert gridManager != null;
 		gridManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
@@ -330,7 +330,7 @@ public abstract class WISERRVAdapter<T, V extends WISERHolder> extends RecyclerV
 
 	@Override public void onViewAttachedToWindow(@NonNull WISERHolder holder) {
 		super.onViewAttachedToWindow((V) holder);
-		if (isStaggerManage(holder)) handleLayoutIfStaggeredGridLayout(holder, holder.getLayoutPosition());
+		if (isStaggerManage(holder) && isFooter) handleLayoutIfStaggeredGridLayout(holder, holder.getLayoutPosition());
 	}
 
 	// 判断是否GridLayoutManage
